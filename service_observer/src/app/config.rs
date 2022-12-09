3d25682f53bc6d31f31ec53ccc0cac54259b06dd
Jontze <42588836+jontze@users.noreply.github.com>
2022-12-09 13:22:35 +0100

    refactor(config): Export functions to extract db and config path

--- service_observer/src/app/config.rs
+++ service_observer/src/app/config.rs
@@ -1,4 +1,7 @@
-use std::fs::{self, File};
+use std::{
+    fs::{self, File},
+    path::PathBuf,
+};
 
 use super::constants::APP_NAME;
 
@@ -7,22 +10,40 @@ pub(crate) fn ensure_app_files_exist() {
     create_database();
 }
 
-fn create_config() {
+pub(crate) fn get_db_path() -> PathBuf {
+    let mut data_file_path = get_db_dir();
+    data_file_path.push("data.sqlite");
+    data_file_path
+}
+
+pub(crate) fn get_config_path() -> PathBuf {
+    let mut config_file_path = get_config_dir();
+    config_file_path.push("data.sqlite");
+    config_file_path
+}
+
+fn get_config_dir() -> PathBuf {
     let mut config_file_path = dirs::config_dir().expect("Unable to get user config path");
     config_file_path.push(&APP_NAME);
+    config_file_path
+}
+
+fn get_db_dir() -> PathBuf {
+    let mut data_file_path = dirs::data_dir().expect("Unable to get user data path");
+    data_file_path.push(&APP_NAME);
+    data_file_path
+}
+
+fn create_config() {
     // Ensure directory exists
-    fs::create_dir_all(&config_file_path).expect("To create config directory");
-    config_file_path.push("config.toml");
+    fs::create_dir_all(&get_config_dir()).expect("To create config directory");
     // Ensure file exists
-    File::create(config_file_path).expect("Config to be created");
+    File::create(&get_config_path()).expect("Config to be created");
 }
 
 fn create_database() {
-    let mut data_file_path = dirs::data_dir().expect("Unable to get user data path");
-    data_file_path.push(&APP_NAME);
     // Ensure directory exists
-    fs::create_dir_all(&data_file_path).expect("To create database directory");
-    data_file_path.push("data.sqlite");
+    fs::create_dir_all(&get_db_dir()).expect("To create database directory");
     // Ensure file exists
-    File::create(data_file_path).expect("Database to be created");
+    File::create(&get_db_path()).expect("Database to be created");
 }
