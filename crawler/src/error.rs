2f386bad76c68e90a7b8f891a14d4e09607ab1ae
Jontze <42588836+jontze@users.noreply.github.com>
2022-12-11 14:24:28 +0100

    chore(crawler): Add db interaction error

--- crawler/src/error.rs
+++ crawler/src/error.rs
@@ -2,4 +2,6 @@
 pub enum CrawlerError {
     #[error("Failed to interact with shodan")]
     ScannerInteraction(#[from] ip_geolocation::IpScannerError),
+    #[error("Failed to interact with the database")]
+    DbInteraction(#[from] sea_orm::DbErr),
 }
