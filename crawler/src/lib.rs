f6efce9f142e06f5a61f0b9250720308c0ef2d3f
Jontze <42588836+jontze@users.noreply.github.com>
2022-12-11 15:46:33 +0100

    feat(crawler): Save fetched geolocations in db to reduce api requests

--- crawler/src/lib.rs
+++ crawler/src/lib.rs
@@ -1,10 +1,13 @@
-use std::net::Ipv4Addr;
-
 use async_trait::async_trait;
-
+use entity::{geolocation, ip, prelude::*};
 use ip_geolocation::IpScanner;
 use migration::MigratorTrait;
-use sea_orm::{Database, DatabaseConnection};
+use sea_orm::{prelude::*, QueryOrder};
+use sea_orm::{
+    ActiveModelTrait, ActiveValue, ColumnTrait, Database, DatabaseConnection, EntityTrait,
+    QueryFilter, QuerySelect,
+};
+use std::net::Ipv4Addr;
 
 mod error;
 pub use error::CrawlerError;
@@ -31,22 +34,63 @@ impl AppCrawler for Crawler {
     }
 
     async fn geolocation(&self, ipv4: &Ipv4Addr) -> Result<(f64, f64), CrawlerError> {
-        // TODO: Check if ip already in db and if an geolocation exists that is younger than 24h
-        // --> If Exists -> take db saved location and return
-        // else
-        // Fetch geolocation --> Save in db and return value
-        let geolocation = self.scanner.clone().ip_geolocation(&ipv4).await?;
-        Ok((geolocation.latitude, geolocation.longitude))
+        // Check if IP already in DB
+        let saved_ip = Ip::find()
+            .filter(ip::Column::Ipv4.eq(&*ipv4.to_string()))
+            .one(&self.db)
+            .await?;
+        if let Some(db_ip) = saved_ip {
+            // Get saved locations
+            let geolocations = db_ip
+                .find_related(geolocation::Entity)
+                .order_by_desc(geolocation::Column::Id)
+                .limit(1)
+                .all(&self.db)
+                .await?;
+            // TODO: Check age of response, and refetch location of older than 24hours / 1 week / ...
+            let geolocation::Model {
+                id: _,
+                latitude,
+                longitude,
+                created: _,
+                ip_id: _,
+            } = geolocations.get(0).unwrap();
+            // Return latest saved location
+            Ok((latitude.to_owned(), longitude.to_owned()))
+        } else {
+            // If not saved, fetch and save ip with geolocations
+            let fetched_geolocation = self.scanner.clone().ip_geolocation(&ipv4).await?;
+            let ip_to_save = ip::ActiveModel {
+                ipv4: ActiveValue::Set(ipv4.to_string()),
+                ..Default::default()
+            };
+            let saved_ip = ip_to_save.insert(&self.db).await?;
+            let location_to_save = geolocation::ActiveModel {
+                ip_id: ActiveValue::Set(saved_ip.id),
+                latitude: ActiveValue::Set(fetched_geolocation.latitude),
+                longitude: ActiveValue::Set(fetched_geolocation.longitude),
+                ..Default::default()
+            };
+            let saved_location = location_to_save.insert(&self.db).await?;
+            Ok((saved_location.latitude, saved_location.longitude))
+        }
     }
 }
 
 #[cfg(test)]
 mod test {
+    use std::net::Ipv4Addr;
+
     use crate::{AppCrawler, Crawler};
 
     #[tokio::test]
     async fn test_connection() {
         let path = "/home/jontze/.local/share/service_observer/data.sqlite";
-        let crawler = Crawler::new(path, "").await;
+        // let crawler = Crawler::new(path, &std::env::var("SHODAN_TOKEN").unwrap()).await;
+        // let ret = crawler
+        //     .geolocation(&Ipv4Addr::new(49, 51, 19, 172))
+        //     .await
+        //     .unwrap();
+        // println!("{ret:#?}")
     }
 }
